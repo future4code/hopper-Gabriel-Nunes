1647bfc67b727a805d0efd1b977eed7adbd3f642
Gabriel Nunes <gabrielnunes1999@hotmail.com>
2022-02-08 21:00:35 -0300

    Entrega do exercício de variáveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,59 @@
+//Interpretação de Código
+// 10
+// 10, 5
+
+// 10, 10, 10
+
+// horasTrabalhadasDia
+// valorDia
+
+//Escrita de Código
+
+/* const nome 
+const idade
+
+console.log (typeof nome, idade) 
+//O comando typeof não conseguiu ser ativado pois não tem nenhuma informação nas variáveis. */
+
+/* const nome = prompt ("Qual o seu nome?")
+const idade = prompt ("Qual a sua idade?")
+
+console.log (typeof nome)
+console.log (typeof idade)
+// O comando typeof foi executado sem erros pois foram atribuídos valores às variáveis.
+
+console.log ("Olá", nome,",você tem",idade,"anos.") */
+
+/* const p1 = "Você gosta de Pizza?"
+const p2 = "Você mora no Brasil?"
+const p3 = "Você fala francês?"
+
+const r1 = "SIM"
+const r2 = "SIM"
+const r3 = "NÃO"
+
+console.log (p1,r1)
+console.log (p2,r2)
+console.log (p3,r3) */
+
+/* let a = 10
+let b = 25
+
+c = a
+a = b
+b = c
+
+
+console.log ("O novo valor de a é:",a,)
+console.log ("O novo valor de b é:",b,)*/
+
+//DESAFIO
+
+/* const n1 = Number(prompt("DIGITE UM NÚMERO:"))
+const n2 = Number(prompt("DIGITE OUTRO NÚMERO:"))
+
+x = n1+n2
+y = n1*n2
+
+console.log ("O primeiro número somado ao segundo número resulta em:",x,".")
+console.log ("O primeiro número multiplicado pelo segundo número resulta em:",y,".") */
