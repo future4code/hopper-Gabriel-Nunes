b9834f436764ad5cded3bf62fc38687bd001d871
Gabriel Nunes <gabrielnunes1999@hotmail.com>
2022-02-17 20:01:08 -0300

    Entrega Exercicio Funções

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,111 @@
+// EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO
+//  1
+// a. 10, 50
+// b. Nada, pois a função não pede para mostrar nada no console
+
+// 2
+// a. Essa função serve para formatar o texto para letras minusculas e 
+// para pesquisar na frase se há determinada palavra.
+
+// b. i = true
+// ii = true
+// iii = true
+
+// ########## //
+
+// EXERCÍCIO DE ESCRITA DE CÓDIGO
+//1 
+// a.
+
+// const nome = "Gabriel"
+// const idade = 22
+// const cidade = "Caruaru"
+// const profissao = "Designer Gráfico"
+
+// function imprimeMensagem() {
+// console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`)
+
+// }
+
+// imprimeMensagem()
+
+// b.
+
+// function imprimeMensagem(nome,idade,cidade,profissao) {
+
+// console.log("Eu sou",nome,"tenho", idade,"anos, moro em", cidade, "e sou", profissao,".")
+
+// }
+
+// imprimeMensagem("Gabriel", "22", "Caruaru", "Designer Gráfico")
+
+//2
+//a
+// function soma(num1,num2) {
+
+// return num1+num2
+
+// }
+
+// console.log(soma(7,7))
+
+//b
+// function boolean(num1,num2) {
+
+// return num1 >= num2
+
+// }
+
+// console.log(boolean(7,8))
+
+//c
+//  function parOuImpar(numero) {
+
+//  return (numero%2) === 0
+
+//  }
+
+// console.log(parOuImpar(2))
+
+//d
+//  function tamanhoEMaiusculas(mensagem) {
+
+// console.log(mensagem.length, mensagem.toUpperCase())
+
+//  }
+
+// tamanhoEMaiusculas("Meu nome é Gabriel") 
+
+//3
+// function soma(a,b) {
+
+// return a+b
+
+// }
+
+// function subtracao(a,b) {
+
+// return a-b
+
+// }
+
+// function multiplicacao(a,b) {
+ 
+// return a*b
+
+// }
+
+// function divisao(a,b) {
+
+// return a/b
+
+// }
+
+// const a = Number(prompt("Digite um numero: "))
+// const b = Number(prompt("Digite outro numero: "))
+
+// console.log("Soma:",soma(a,b))
+// console.log("Subtracao:",subtracao(a,b))
+// console.log("Multiplicação:",multiplicacao(a,b))
+// console.log("Divisão:",divisao(a,b))
+
