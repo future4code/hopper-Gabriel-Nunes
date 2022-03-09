ac28477967590f9e5af70df7578a38d015826f8e
Gabriel Nunes <gabrielnunes1999@hotmail.com>
2022-03-08 21:54:21 -0300

    Entrega do Exercicio Lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,29 @@
+// EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO
+//1 - 10
+
+//2 a) 19, 21, 23, 25, 27, 30
+//  b) Não
+
+//3 *, **, ***, ****
+
+// EXERCÍCIO DE ESCRITA DE CÓDIGO
+
+//1
+
+// const bichos = Number(prompt("Quantos bichos de estimação você tem?"));
+// const nomePets = []
+
+// if (bichos < 1) {
+//     console.log("Que pena! Você pode adotar um pet!");
+// } else if (bichos > 0) {
+//     for (let i = 0; i < bichos; i++) {
+//         let input = prompt("Quais os nomes dos seus pets?");
+//         nomePets.push(input);
+//     }
+
+// }
+
+// console.log(`Os nomes dos Pets são: ${nomePets}`)
+
+//2 - FAZENDO
+
