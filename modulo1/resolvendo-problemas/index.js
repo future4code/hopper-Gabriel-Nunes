55d74780a6ffa55fab8db3b9218a1622abc0921e
Gabriel Nunes <gabrielnunes1999@hotmail.com>
2022-03-18 12:55:04 -0300

    Resolvendo Problemas

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,31 @@
+// RESOLVENDO PROBLEMAS //
+
+// 1º Passo: *Ter Atenção no Enunciado*
+/* Neste passo, é fundamental ler o enunciado com atenção, questionar sobre 
+o que o código deve fazer e identificar informações únicas sobre o que pede
+o problema. */
+
+// 2º Passo: *Retirar Informações Relevantes*
+/* Aqui, é importante separar palavras chave e colocá-las, de preferência, como nomes
+de variáveis e os verbos como nomes de funções, e também, verificar se existe alguma
+condição para a existência do código. */
+
+// 3º Passo: *Pensar nas Entradas e Saídas*
+/* No passo 3, devemos pensar em como receberemos os dados e quais serão esses dados. A partir
+disso, pensamos em como colocar isso em um código e criar uma função com as entradas e saídas. */
+
+// 4º Passo: *Fazer Um Esquema Visual*
+/* Este passo consiste em criarmos um diagrama com a lógica do código, pensando em vários cenários
+diferentes para o código saber lidar com vários tipos de respostas diferentes. */
+
+// 5º Passo: *Criar Um Código Feio*
+/* No passo 5, criaremos um "códig feio", que seria um código "explicativo" sobre o que o nosso código
+final deve fazer, facilitando assim a aplicação da lógica e da sintaxe. */
+
+// 6º Passo: *Testar: Força-Bruta*
+/* Este passo consiste em buscar uma solução óbvia mas pouco otimizada, escrevendo o código no nosso
+idioma e logo após, transcrevendo para a linguagem de programação, se aproximando do nosso código final. */
+
+// 7º Passo: *Refatorar*
+/* No último passo, buscamos aperfeiçoar o nosso código, deixando-o mais "clean", com nomes de variáveis
+mais lógicos por exemplo. */
