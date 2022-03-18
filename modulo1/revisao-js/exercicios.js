6ccf460fd1c9a8abe36292eb4784bb899444bc80
Gabriel Nunes <gabrielnunes1999@hotmail.com>
2022-03-18 16:55:33 -0300

    Exercicio 13A

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -101,7 +101,18 @@ function retornaPessoaAnonimizada(pessoa) {
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
+    let pessoasAutorizadas = []
+    for(let pessoa of pessoas) {
+        if (pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60) {
+            pessoasAutorizadas.push(pessoa)
 
+
+        }
+
+        
+
+    }
+   return pessoasAutorizadas
 }
 
 // EXERCÍCIO 13B
